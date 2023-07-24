b24e9daa98d21e66a1eaaf94e694d7dcf0091db1
Hsinan Chen <chen.hsinan2007@gmail.com>
2023-07-23 22:10:56 -0700

    update

---
+++ geeksforgeeks/Master C++ Programming - Complete Beginner to Advanced/Vector/vector2.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <vector> 
+  
+using namespace std; 
+  
+int main() 
+{ 
+    vector<int> v{10, 5, 20}; 
+  
+    for(int x : v){
+        cout << x << " ";  
+    }
+    
+    return 0; 
+}
+
---
+++ geeksforgeeks/Master C++ Programming - Complete Beginner to Advanced/Vector/vector8.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <vector> 
+  
+using namespace std; 
+  
+int main() 
+{ 
+    vector<int> v{10, 5, 20, 15} ; 
+    v.clear();
+    
+    cout << v.size() << " ";
+  
+    if(v.empty() == true)
+        cout << "Empty";
+    else
+        cout << "Not Empty";
+    
+    return 0; 
+}
+
