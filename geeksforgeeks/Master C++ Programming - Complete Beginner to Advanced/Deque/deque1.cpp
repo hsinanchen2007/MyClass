04b8f83a9411b4bdc6a69208b97d809371baf624
Hsinan Chen <chen.hsinan2007@gmail.com>
2023-07-24 22:00:51 -0700

    update

---
+++ geeksforgeeks/Master C++ Programming - Complete Beginner to Advanced/Deque/deque1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h> 
+#include <climits>
+#include <deque>
+using namespace std;
+
+
+int main()
+{
+	deque<int> dq = {10, 20, 30};
+
+	dq.push_front(5);
+	dq.push_back(50);
+
+	for(auto x: dq)
+		cout << x << " ";
+
+	cout << dq.front() << " " << dq.back();
+
+	
+	return 0;
+}
+
