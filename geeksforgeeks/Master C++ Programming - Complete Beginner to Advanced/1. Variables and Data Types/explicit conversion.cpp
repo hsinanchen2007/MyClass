8fc9c2c7c807b9ec0e5d19ac208584f49a288918
Hsinan Chen <chen.hsinan2007@gmail.com>
2023-08-02 20:45:58 -0700

    update

---
+++ geeksforgeeks/Master C++ Programming - Complete Beginner to Advanced/1. Variables and Data Types/explicit conversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 15, y = 2;
+    double z1 = x/y;
+    cout << z1 << '\n';
+    double z2 = double(x)/y;
+    cout << z2 << '\n';
+    return 0;
+}
