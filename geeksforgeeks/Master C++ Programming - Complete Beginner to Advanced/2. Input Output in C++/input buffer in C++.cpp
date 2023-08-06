7747e762bf5b72bdde0c7035acbd06f47def02a9
Hsinan Chen <chen.hsinan2007@gmail.com>
2023-08-05 20:55:35 -0700

    update

---
+++ geeksforgeeks/Master C++ Programming - Complete Beginner to Advanced/2. Input Output in C++/input buffer in C++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter the first integer: \n";
+    cin >> x;
+    cout << "Enter the second integer: \n";
+    cin >> y;
+    cout << "Multiplication is: " << (x*y) << '\n';
+    return 0;
+}
+
