b2ad02a8cdc5d639b01943276170290311025365
Jason Chen <chen.hsinan2007@gmail.com>
2024-05-27 10:16:56 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第14课 - 进阶面向对象（下）/class/code/14-1/14-1.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+// For struct, it is by default public inhertance
+// The main difference in C++ between struct and class
+// is that struct by default is public, but class
+// by default is private. 
+struct Biology {
+    bool living;
+};
+
+struct Animal : Biology {
+    bool movable;
+    void findFood() { }
+};
+
+struct Plant : Biology {
+    bool growable;
+};
+
+struct Beast : Animal {
+    void sleep() { }
+};
+
+struct Human : Animal {
+    void sleep() { }
+    void work() { }
+};
+
+int main()
+{
+    Human H;
+
+    // Because struct by default is public inheritance,
+    // we can access functions and members from Human, 
+    // to Animal, to Biology
+    H.findFood();
+    H.movable;
+    H.living;
+
+    return 0;
+}
