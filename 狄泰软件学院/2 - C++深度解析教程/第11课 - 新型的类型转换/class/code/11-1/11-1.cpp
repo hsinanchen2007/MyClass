c47332e66bf99d435f71be37876dcee246ede20d
Jason Chen <chen.hsinan2007@gmail.com>
2024-05-26 22:11:16 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第11课 - 新型的类型转换/class/code/11-1/11-1.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+typedef void(PF)(int);
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    // The root cause is below fake address
+    // which is not real one in the program
+    // This can cause any fatal or unexpected
+    // result 
+    int v = 0x12345;
+
+    PF* pf = (PF*)v;
+    char c = char(v);
+    Point* p = (Point*)v;
+
+    pf(5);
+
+    printf("p->x = %d\n", p->x);
+    printf("p->y = %d\n", p->y);
+
+    return 0;
+}
---
+++ 狄泰软件学院/2 - C++深度解析教程/第11课 - 新型的类型转换/class/code/11-2/11-2.cpp
@@ -0,0 +1,74 @@
+﻿#include <stdio.h>
+
+void static_cast_demo()
+{
+    int i = 0x12345;
+    char c = 'c';
+    int* pi = &i;
+    char* pc = &c;
+
+    c = static_cast<char>(i);
+
+    // static_Cast<> cannot be used for basic type pointer conversion
+    //pc = static_cast<char*>(pi);
+}
+
+void const_cast_demo()
+{
+    const int& j = 1;
+    int& k = const_cast<int&>(j);
+
+    const int x = 2;
+    int& y = const_cast<int&>(x);
+
+    // const_case<> must use on reference or pointer
+    // int z = const_cast<int>(x);
+
+    k = 5;
+
+    printf("k = %d\n", k);
+    printf("j = %d\n", j);
+
+    y = 8;
+
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("&x = %p\n", &x);
+    printf("&y = %p\n", &y);
+}
+
+void reinterpret_cast_demo()
+{
+    int i = 0;
+    char c = 'c';
+    int* pi = &i;
+    char* pc = &c;
+
+    pc = reinterpret_cast<char*>(pi);
+    pi = reinterpret_cast<int*>(pc);
+
+    // reinterpret_cast<> is used to convert between pointers or 
+    // int and pointer only
+    // pi = reinterpret_cast<int*>(i);
+    // c = reinterpret_cast<char>(i);
+}
+
+void dynamic_cast_demo()
+{
+    int i = 0;
+    int* pi = &i;
+
+    // dynamic_case<> is mainly used within parent/child classes
+    // and virtual function is needed
+    // char* pc = dynamic_cast<char*>(pi);
+}
+
+int main()
+{
+    static_cast_demo();
+    const_cast_demo();
+    reinterpret_cast_demo();
+    dynamic_cast_demo();
+
+    return 0;
+}
