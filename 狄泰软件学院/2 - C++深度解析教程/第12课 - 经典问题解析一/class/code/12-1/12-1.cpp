500f69fbf37b958c4e46a01b48377efdd1f1dd25
Jason Chen <chen.hsinan2007@gmail.com>
2024-05-26 22:44:15 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第12课 - 经典问题解析一/class/code/12-1/12-1.cpp
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    // x cannot be changed
+    const int x = 1;
+
+    // rx cannot be changed, and also create a new variable location
+    // Its address will be different than x
+    const int& rx = x;
+
+    // Now by using const_cast<>, make nrx can be changed 
+    int& nrx = const_cast<int&>(rx);
+    nrx = 5;
+
+    printf("x = %d\n", x);
+    printf("rx = %d\n", rx);
+    printf("nrx = %d\n", nrx);
+    printf("&x = %p\n", &x);
+    printf("&rx = %p\n", &rx);
+    printf("&nrx = %p\n", &nrx);
+
+    // By using volatile, it makes y is not listed as const lookup table
+    // so when we try to get its value, it will return real one if changed
+    volatile const int y = 2;
+
+    // make y can be changed now
+    int* p = const_cast<int*>(&y);
+    *p = 6;
+
+    // Here, y will return real value, not from const lookup table
+    printf("y = %d\n", y);
+    printf("p = %p, %d\n", p, *p);
+
+    // similar to y
+    const int z = y;
+
+    // make z can be changed now
+    p = const_cast<int*>(&z);
+    *p = 7;
+
+    printf("z = %d\n", z);
+    printf("p = %p\n", p);
+
+    // c and rc are pointed to same location, and can be changed
+    char c = 'c';
+    char& rc = c;
+
+    // trc will be initialized into const lookup table and
+    // cannot be changed in code
+    const int& trc = c;
+
+    rc = 'a';
+
+    // will show updated one "a"
+    printf("c = %c %p\n", c, &c);
+    printf("rc = %c %p\n", rc, &rc);
+
+    // will still point to original one from const lookup table
+    printf("trc = %c\n", trc);
+
+    return 0;
+}
---
+++ 狄泰软件学院/2 - C++深度解析教程/第12课 - 经典问题解析一/class/code/12-2/12-2.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int a = 1;
+
+struct SV
+{
+    int& x;
+    int& y;
+    int& z;
+};
+
+int main()
+{
+    int b = 2;
+    int* pc = new int(3);
+    SV sv = { a, b, *pc };
+
+    // Below statement cannot work because *pc won't guarantee that its address will be
+    // in the same order of a and b
+    // int& array[] = { a, b, *pc }; // &array[1] - &array[0] = ?  Expected ==> 4
+
+    // But if we put same data members into struct, it works as struct doesn't require
+    // all data members should be saved in the order with same distance
+    printf("&sv.x = %p\n", &sv.x);
+    printf("&sv.y = %p\n", &sv.y);
+    printf("&sv.z = %p\n", &sv.z);
+
+    delete pc;
+
+    return 0;
+}
