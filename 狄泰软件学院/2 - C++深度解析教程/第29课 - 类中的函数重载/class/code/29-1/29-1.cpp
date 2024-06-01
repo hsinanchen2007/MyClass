0fb4bcc380441d17110e1f065fbc6506d0b8004c
Jason Chen <chen.hsinan2007@gmail.com>
2024-06-01 15:29:40 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第29课 - 类中的函数重载/class/code/29-1/29-1.cpp
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+
+class Test
+{
+    int i;
+public:
+    // constructor without parameter
+    Test()
+    {
+        printf("Test::Test()\n");
+        this->i = 0;
+    }
+
+    // constructor with one parameter
+    Test(int i)
+    {
+        printf("Test::Test(int i)\n");
+        this->i = i;
+    }
+
+    // copy constructor
+    Test(const Test& obj)
+    {
+        printf("Test(const Test& obj)\n");
+        this->i = obj.i;
+    }
+
+    // static member function of this class
+    static void func()
+    {
+        printf("void Test::func()\n");
+    }
+
+    // class object member function
+    void func(int i)
+    {
+        printf("void Test::func(int i), i = %d\n", i);
+    }
+
+    int getI()
+    {
+        return i;
+    }
+};
+
+// global function
+void func()
+{
+    printf("void func()\n");
+}
+
+// global function overloaded with one parameter
+void func(int i)
+{
+    printf("void func(int i), i = %d\n", i);
+}
+
+int main()
+{
+    func();
+    func(1);
+
+    Test t;        // Test::Test()
+    Test t1(1);    // Test::Test(int i)
+    Test t2(t1);   // Test(const Test& obj)
+
+    func();        // void func()
+    Test::func();  // void Test::func()
+
+    func(2);       // void func(int i), i = 2;
+    t1.func(2);    // void Test::func(int i), i = 2
+    t1.func();     // void Test::func()
+
+    return 0;
+}
---
+++ 狄泰软件学院/2 - C++深度解析教程/第29课 - 类中的函数重载/class/code/29-2/29-2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+// Need to add this _CRT_SECURE_NO_WARNINGS to compile it in VC 2022
+// Under "Project -> Properties -> C/C++ -> Preprocessor -> Preprocessor Definitions" 
+// then add _CRT_SECURE_NO_WARNINGS
+// Note that C didn't support overload, here we add one more argument
+// with same function name, so it will call real C library function
+// within this function
+char* strcpy(char* buf, const char* str, unsigned int n)
+{
+    return strncpy(buf, str, n);
+}
+
+int main()
+{
+    const char* s = "D.T.Software";
+    char buf[8] = { 0 };
+
+    // strcpy(buf, s);
+    strcpy(buf, s, sizeof(buf) - 1);
+
+    printf("%s\n", buf);
+
+    return 0;
+}
