b5bbb83de456b37ae5564b859daf755fe68d9476
Jason Chen <chen.hsinan2007@gmail.com>
2024-06-04 09:57:30 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第42课 - 类型转换函数（下）/class/code/42-1/42-1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Test
+{
+    int mValue;
+public:
+    Test(int i = 0)
+    {
+        mValue = i;
+    }
+    int value()
+    {
+        return mValue;
+    }
+
+    // This is the special class converter 
+    // that will convert this given class to 
+    // an integer.
+    // Note that there is no return type
+    // here so it is NOT operator overload
+    operator int()
+    {
+        return mValue;
+    }
+};
+
+int main()
+{
+    Test t(100);
+
+    // Below statement is like 
+    // int i = t.operator int();
+    // so it will try to convert
+    // Test class object t to an
+    // integer
+    int i = t;
+
+    cout << "t.value() = " << t.value() << endl;
+    cout << "i = " << i << endl;
+
+    return 0;
+}
---
+++ 狄泰软件学院/2 - C++深度解析教程/第42课 - 类型转换函数（下）/class/code/42-2/42-2.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Test;
+
+class Value
+{
+    int vv;
+public:
+    // Modify below constructor that can take
+    // mValue from Test class object, then
+    // do something changed in Value class object
+    Value(int v) : vv(v)
+    {
+        cout << "Value(int v) : vv(v)" << endl;
+        vv = vv + 1;
+        cout << vv << endl;
+    }
+    explicit Value(Test& t)
+    {
+        cout << "explicit Value(Test& t)" << endl;
+    }
+};
+
+class Test
+{
+    int mValue;
+public:
+    Test(int i = 0)
+    {
+        cout << "Test(int i = 0)" << endl;
+        mValue = i;
+    }
+    int value()
+    {
+        cout << "int value()" << endl;
+        return mValue;
+    }
+
+    // the common syntax of converter:
+    //    operator type()
+    // No return, no argument
+    operator Value()
+    {
+        cout << "operator Value()" << endl;
+        Value ret(mValue);
+        return ret;
+    }
+};
+
+int main()
+{
+    Test t(100);
+
+    // Here, we are trying to convert
+    // Test class object t to another
+    // class object Value v, below 
+    // statement will be like:
+    // Value v = t.operator Value();
+    // Inside operator Value(), it will
+    // then create a ret of Value, and
+    // return ret
+    // Note that there is no return 
+    // type here, like type converter 
+    // function
+    Value v = t;
+
+    return 0;
+}
