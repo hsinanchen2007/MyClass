9c04da00ebe39fac606944242628d09b59596f8d
Jason Chen <chen.hsinan2007@gmail.com>
2024-06-03 23:07:24 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第41课 - 类型转换函数（上）/class/code/41-1/41-1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    short s = 'a';
+    unsigned int ui = 1000;
+    int i = -2000;
+    double d = i;
+
+    cout << "d = " << d << endl;
+    cout << "ui = " << ui << endl;
+
+    // unsigned int + int, the root cause
+    // is mainly due to i is a nagative value,
+    // when it is converted to unsigned int,
+    // the value will become unexpected value
+    cout << "ui + i = " << ui + i << endl;
+    cout << "(unsigned int)i" << (unsigned int)i << endl;
+
+    if ((ui + i) > 0)
+    {
+        cout << "Positive" << endl;
+    }
+    else
+    {
+        cout << "Negative" << endl;
+    }
+
+    cout << "sizeof(s + 'b') = " << sizeof(s + 'b') << endl;
+
+    return 0;
+}
---
+++ 狄泰软件学院/2 - C++深度解析教程/第41课 - 类型转换函数（上）/class/code/41-2/41-2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Test
+{
+    int mValue;
+public:
+    Test()
+    {
+        mValue = 0;
+    }
+
+    // the "explicit" tells compiler that
+    // do NOT convert the class object
+    // automatically, for example,
+    // "Test t = 15" this won't work
+    // Only "Test t(15)" will work
+    explicit Test(int i)
+    {
+        mValue = i;
+    }
+
+    Test operator + (const Test& p)
+    {
+        Test ret(mValue + p.mValue);
+
+        return ret;
+    }
+
+    int value()
+    {
+        return mValue;
+    }
+};
+
+int main()
+{
+    Test t;
+
+    t = static_cast<Test>(5);    // t = Test(5);
+
+    Test r;
+
+    r = t + static_cast<Test>(10);   // r = t + Test(10);
+
+    cout << r.value() << endl;
+
+    // Below won't work because of "explicit" keyword
+    // Test s = 15;
+    //
+    // Below will work as it completely match constructor
+    // https://en.cppreference.com/w/cpp/language/explicit
+    //
+    Test s(15);
+
+    return 0;
+}
