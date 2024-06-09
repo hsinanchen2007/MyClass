a4106b9dcd5227ce5f182c350f74f2d90d90c1cc
Jason Chen <chen.hsinan2007@gmail.com>
2024-06-08 19:19:16 -0700

    update

---
+++ 狄泰软件学院/2 - C++深度解析教程/第45课 - 不同的继承方式/class/code/45-1/45-1.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Within parent and child classes, 
+// child class can access public and 
+// protected members, but for objects,
+// they can still access public ones
+class Parent
+{
+private:
+    int m_private;
+protected:
+    int m_protected;
+public:
+    int m_public;
+
+    Parent(int v1, int v2, int v3) {
+        m_private = v1;
+        m_protected = v2;
+        m_public = v3;
+    }
+};
+
+// All access remains same in Parent
+class Child_A : public Parent
+{
+public:
+    Child_A() : Parent(1, 2, 3)
+    {}
+    // public inhert, parent class
+    // remain its original access level
+    void Test(void) {
+        // m_private = 4; cannot access
+        m_protected = 5;
+        m_public = 6;
+        return;
+    }
+};
+
+// Highest access of Parent now is protected 
+class Child_B : protected Parent
+{
+public:
+    Child_B() : Parent(1, 2, 3)
+    {}
+    // protected inhert, parent class
+    // can access its public and protected
+    // access level
+    void Test(void) {
+        // m_private = 4; cannot access
+        m_protected = 5;
+        m_public = 6;
+        return;
+    }
+};
+
+// Highest access of Parent now is private
+class Child_C : private Parent
+{
+public:
+    Child_C() : Parent(1, 2, 3)
+    {}
+    // private inhert, parent class
+    // can access its public and protected
+    // access level
+    void Test(void) {
+        // m_private = 4; cannot access
+        m_protected = 5;
+        m_public = 6;
+        return;
+    }
+};
+
+int main()
+{
+    Child_A a;
+    a.m_public = 100;           // still public in a
+    // a.m_protected = 100;
+    // a.m_private = 100;       
+
+    Child_B b;
+    // b.m_public = 100;        // become protected in b
+    // b.m_protected = 100;
+    // b.m_private = 100;
+
+    Child_C c;
+    // c.m_public = 100;        // become private in c
+    // c.m_protected = 100;
+    // c.m_private = 100;
+
+    return 0;
+}
---
+++ 狄泰软件学院/2 - C++深度解析教程/第45课 - 不同的继承方式/class/code/45-2/45-2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Parent
+{
+    // if m_a is private, child class will not
+    // be able to access it
+protected:
+    int m_a;
+protected:
+    int m_b;
+public:
+    int m_c;
+
+    void set(int a, int b, int c)
+    {
+        m_a = a;
+        m_b = b;
+        m_c = c;
+    }
+};
+
+class Child_A : public Parent
+{
+public:
+    void print()
+    {
+        cout << "m_a" << m_a << endl;
+        cout << "m_b" << m_b << endl;
+        cout << "m_c" << m_c << endl;
+    }
+};
+
+class Child_B : protected Parent
+{
+public:
+    void print()
+    {
+        cout << "m_a" << m_a << endl;
+        cout << "m_b" << m_b << endl;
+        cout << "m_c" << m_c << endl;
+    }
+};
+
+class Child_C : private Parent
+{
+public:
+    void print()
+    {
+        cout << "m_a" << m_a << endl;
+        cout << "m_b" << m_b << endl;
+        cout << "m_c" << m_c << endl;
+    }
+};
+
+int main()
+{
+    Child_A a;
+    Child_B b;
+    Child_C c;
+
+    a.m_c = 100;
+    // b.m_c = 100;    // Child_B �����̳��� Parent�� �������е� public ��Աȫ������� protected ��Ա�� �������޷�����
+    // c.m_c = 100;    // Child_C ˽�м̳��� Parent�� �������еĳ�Աȫ������� private ��Ա�� �������޷�����
+
+    a.set(1, 1, 1);
+    // b.set(2, 2, 2);
+    // c.set(3, 3, 3);
+
+    a.print();
+    b.print();
+    c.print();
+
+    return 0;
+}
