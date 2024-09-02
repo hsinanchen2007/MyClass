7be31656a19fb8ab65c7f5fb8bfba94c1e23c6f7
Jason Chen <chen.hsinan2007@gmail.com>
2024-09-02 10:08:34 -0700

    update

--- 狄泰软件学院/0 - C语言基础入门教程/第8课 - 四则运算与关系运算/class/code/8-2.c
+++ 狄泰软件学院/0 - C语言基础入门教程/第8课 - 四则运算与关系运算/class/code/8-2.c
@@ -7,13 +7,11 @@ int main()
     int c = 3;
 
     c = a != b;
-
     c = a - b >= a + b;
-
     printf("c = %d\n", c);
 
+    // c now is 0, not 3 anymore
     c = (a < b) + (c < b);
-
     printf("c = %d\n", c);
 
     return 0;
