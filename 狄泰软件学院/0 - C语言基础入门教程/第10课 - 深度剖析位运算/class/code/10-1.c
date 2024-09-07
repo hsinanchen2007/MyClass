b632746d01421f5a3d3772ea3e37cfef35588faa
Jason Chen <chen.hsinan2007@gmail.com>
2024-09-07 15:33:39 -0700

    update

--- 狄泰软件学院/0 - C语言基础入门教程/第10课 - 深度剖析位运算/class/code/10-1.c
+++ 狄泰软件学院/0 - C语言基础入门教程/第10课 - 深度剖析位运算/class/code/10-1.c
@@ -6,15 +6,18 @@ int main()
     short b = 2;
     int c = a - b;
 
+    printf("c = %d\n", c);          // -1
+
+    // 1001 >> 4 = 1100 -> 1110 -> 1111 -> 1111
     c = c >> 4;
 
-    printf("c = %d\n", c);
+    printf("c = %d\n", c);          // -1
 
     c = c * -1 * 16 >> 4;
 
-    printf("c = %d\n", c);
+    printf("c = %d\n", c);          // 1
 
-    printf("c = %d\n", 16 << 2);
+    printf("c = %d\n", 16 << 2);    // 64
 
     return 0;
 }
--- 狄泰软件学院/0 - C语言基础入门教程/第10课 - 深度剖析位运算/class/code/10-2.c
+++ 狄泰软件学院/0 - C语言基础入门教程/第10课 - 深度剖析位运算/class/code/10-2.c
@@ -6,23 +6,25 @@ int main()
     short a = c;
     int b = c;
 
-    printf("c = %c\n", c);
-    printf("c = %d\n", c);
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
+    printf("c = %c\n", c);  // A
+    printf("c = %d\n", c);  // 65
+    printf("a = %d\n", a);  // 65
+    printf("b = %d\n", b);  // 65
 
     c = 0x40;
 
-    printf("c = %x\n", c);
-    printf("c = %d\n", c);
+    printf("c = %x\n", c);  // 40
+    printf("c = %d\n", c);  // 64
+
+    // char's range is -128 ~ 127, as the "c << 1" will become 128, which is over its char range
+    // so the value will become -128 instead
+    c = c << 1;
+
+    printf("c = %d\n", c);  // -128
 
     c = c << 1;
 
-    printf("c = %d\n", c);
-
-    c = c << 1;
-
-    printf("c = %d\n", c);
+    printf("c = %d\n", c);  // 0
 
     return 0;
 }
