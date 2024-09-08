2667ab4dba0daa2c73aa09fcd5664223209b9b51
Jason Chen <chen.hsinan2007@gmail.com>
2024-09-07 23:14:53 -0700

    update

--- 狄泰软件学院/0 - C语言基础入门教程/第22课 - 程序中的辅助语句（下）/class/code/22-1.c
+++ 狄泰软件学院/0 - C语言基础入门教程/第22课 - 程序中的辅助语句（下）/class/code/22-1.c
@@ -7,17 +7,17 @@ int main()
 
     a = -c++;  // c -> 2
 
-    printf("a = %d\n", a);
+    printf("a = %d\n", a);      // a = -1
 
-    a = +++c; // +(++c); c -> 3
+    a = +(++c); // +(++c); c -> 3
 
-    printf("a = %d\n", a);
+    printf("a = %d\n", a);      // a = 3
 
     a = -c--;  // c -> 2
 
-    printf("a = %d\n", a);
+    printf("a = %d\n", a);      // a = -3
 
-    printf("c = %d\n", c);
+    printf("c = %d\n", c);      // c = 2
 
     return 0;
 }
