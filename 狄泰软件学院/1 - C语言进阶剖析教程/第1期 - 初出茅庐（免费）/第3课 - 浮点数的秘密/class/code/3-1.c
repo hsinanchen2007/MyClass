7760ca4263daa1531de8357ba10bb3c926eaaaf7
Jason Chen <chen.hsinan2007@gmail.com>
2024-09-12 23:25:33 -0700

    update

--- 狄泰软件学院/1 - C语言进阶剖析教程/第1期 - 初出茅庐（免费）/第3课 - 浮点数的秘密/class/code/3-1.c
+++ 狄泰软件学院/1 - C语言进阶剖析教程/第1期 - 初出茅庐（免费）/第3课 - 浮点数的秘密/class/code/3-1.c
@@ -6,7 +6,10 @@ int main()
     
     unsigned int* p = (unsigned int*)&f;
     
-    printf("0x%08X\n", *p);
+    printf("0x%08X\n", *p);                             // 0x41040000
     
+    printf("sizeof(float) = %d\n", sizeof(float));      // sizeof(float) = 4
+    printf("sizeof(double) = %d\n", sizeof(double));    // sizeof(double) = 8
+
     return 0;
 }
