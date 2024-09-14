e2335112a7a5345e5f2db16fffb9da230fffd860
Jason Chen <chen.hsinan2007@gmail.com>
2024-09-13 22:15:28 -0700

    update

--- 狄泰软件学院/1 - C语言进阶剖析教程/第1期 - 初出茅庐（免费）/第3课 - 浮点数的秘密/class/code/3-2.c
+++ 狄泰软件学院/1 - C语言进阶剖析教程/第1期 - 初出茅庐（免费）/第3课 - 浮点数的秘密/class/code/3-2.c
@@ -6,8 +6,8 @@ int main()
     float f = 3.1415f;
     float fl = 123456789;
     
-    printf("%0.10f\n", f);
-    printf("%0.10f\n", fl);
+    printf("%0.10f\n", f);      // 3.1414999962
+    printf("%0.10f\n", fl);     // 123456792.0000000000
     
     return 0;
 }
