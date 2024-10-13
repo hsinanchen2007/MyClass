51d53f3a5bf9fab280379b5c6b8d1d74e9f21993
Jason Chen <chen.hsinan2007@gmail.com>
2024-10-12 22:46:02 -0700

    update

--- 狄泰软件学院/1 - C语言进阶剖析教程/第2期 - 风云叱咤/第39课 - 程序中的三国天下/class/code/39-2.c
+++ 狄泰软件学院/1 - C语言进阶剖析教程/第2期 - 风云叱咤/第39课 - 程序中的三国天下/class/code/39-2.c
@@ -8,14 +8,14 @@ void f()
 {
     static int g_vl = 3;
     
-    printf("%p\n", &g_vl);
+    printf("%p\n", &g_vl);  // 01003008
 }
 
 int main()
 {
-    printf("%p\n", &g_v);
+    printf("%p\n", &g_v);   // 01003000
     
-    printf("%p\n", &g_vs);
+    printf("%p\n", &g_vs);  // 01003004
     
     f();
     
