6c452210a35d7de381b0d25dedb30fa5626b9fad
Jason Chen <chen.hsinan2007@gmail.com>
2024-12-22 22:30:11 -0800

    update

--- 尚硅谷宋红康C语言精讲/chapter 1/5.c
+++ 尚硅谷宋红康C语言精讲/chapter 1/5.c
@@ -3,5 +3,8 @@
 int main()                      //定义主函数
 {                               //函数开始的标志
     printf("Hello World\n");    //输出所指定的一行信息
+
+    printf("%f%%\n", 100.0 / 3.33); // printf函数的使用方法 with %% to print % itself
+
     return 0;                   //函数执行完毕时返回函数值0
 }                               //函数结束的标志
